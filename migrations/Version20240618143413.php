270ef23b0a4cefa7889de6686b18a3ead5cd7fe3
Nass <nassim.talal@hotmail.com>
2024-06-25 15:51:58 +0200

    first commit

--- migrations/Version20240618143413.php
+++ migrations/Version20240618143413.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240617143235 extends AbstractMigration
+final class Version20240618143413 extends AbstractMigration
 {
     public function getDescription(): string
     {
